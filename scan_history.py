f28dbcc5cde601bd0204c560937deb7cd3564e1b
Chilandar <genghiz11621227@gmail.com>
2026-09-08 01:02:21 +0330

    Update scan_history.py

--- scan_history.py
+++ scan_history.py
@@ -17,7 +17,7 @@
 import datetime
 import config
 from telegram_client import get_client, load_json, save_json, send_bot_message, resolve_source_entity
-from text_utils import detect_category, text_hash_for_dedupe
+from text_utils import detect_category, text_hash_for_dedupe, get_visible_content_length, MIN_VISIBLE_CONTENT_LEN
 from ai_classify import classify_with_gemini
 
 import pytz
